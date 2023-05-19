36e64a5d98cfc0f3a845f4b3ebf0d2c35ef3a3e1
JoshuaAbulu <abulujoshua99@gmail.com>
2023-05-19 21:36:36 +0100

    added static again

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,12 @@ require('dotenv').config()
 const blogRoutes = require('./routes/blogRoutes')
 const userRoutes = require('./routes/userRoutes')
 const cors = require('cors')
+const path = require('path');
 
 const app = express()
 
 app.use(cors())
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 app.use(express.json())
 app.use('/api/blogs', blogRoutes)
