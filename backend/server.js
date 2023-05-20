d919c5a238b0df2dba7922a5eff97d72f4895fd0
JoshuaAbulu <abulujoshua99@gmail.com>
2023-05-20 08:52:42 +0100

    added slash

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('/blog', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
-})
+
 
 app.use(express.json())
 app.use('/api/blogs', blogRoutes)
