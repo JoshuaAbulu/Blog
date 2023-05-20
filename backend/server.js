c8acda2dc47eef0b16772f1aad9d169a2e8d93a7
JoshuaAbulu <abulujoshua99@gmail.com>
2023-05-20 07:41:57 +0100

    added route

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const path = require('path');
 const app = express()
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, '../frontend/public/')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
+})
 
 app.use(express.json())
 app.use('/api/blogs', blogRoutes)
